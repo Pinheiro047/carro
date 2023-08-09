af4d0f7ac254c5c0569f9f684534584346d6fff5
FELIPE TADEU PINHEIRO <felipe_t_pinheiro@estudante.sesisenai.org.br>
2023-08-09 08:16:36 -0300

    compartilhando

---
+++ Util.js
@@ -0,0 +1,114 @@
+//classe base
+class obj{
+    constructor(x,y,w,h,attrib){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.attrib = attrib
+    }
+
+    dir = 0
+
+    des_obj(){
+        let img = new Image()
+        img.src = this.attrib
+        des.drawImage(img,this.x,this.y,this.w,this.h)
+}
+    }
+
+class F1 extends obj{
+    dir = 0
+    pts = 0
+
+
+    mover(){
+        this.x += this.dir
+        if(this.x <= 12){
+            this.x = 10
+        }else if(this.x >= 524){
+            this.x = 524
+        }
+    }
+
+    colid(objeto){
+        if((this.x < objeto.x + objeto.w)&&
+           (this.x - this.w >objeto.x)&&
+           (this.y < objeto.y + objeto.w)&&
+           (this.y + this.w > objeto.y)){
+            return true
+           }else{
+            return false
+           }
+}
+
+    point(objeto){
+        if(objeto.y > 601){
+            return true
+        }
+    }
+
+}
+
+class Car extends obj{
+    mover(){
+        this.y += 4
+
+        if(this.y >= 610){
+        this.y = -100
+
+        this.x = (Math.random() * (540 - 2 + 1) + 2)
+        }
+    }
+}
+
+class Est_l extends obj{
+    des_obj(){
+    des.beginPath()   // Para iniciar objeto e poder tornar objeto
+    des.lineWidth = '5'  //Espessura da linha
+    des.fillStyle = this.attrib  //Cor da pintura
+    des.strokeStyle = 'white'  //Cor da linha
+    des.rect(this.x,this.y,this.w,this.h)
+    des.closePath() //Para fechar e poder tornar um objeto
+    des.stroke()  //Risca linha
+    des.fill()  //Pinta Área
+    }
+}
+class Est_c extends obj{
+
+    des_obj(){
+    des.beginPath()   // Para iniciar objeto e poder tornar objeto
+    des.lineWidth = '5'  //Espessura da linha
+    des.fillStyle = this.attrib  //Cor da pintura
+    des.strokeStyle = 'yellow'  //Cor da linha
+    des.rect(this.x,this.y,this.w,this.h)
+    des.closePath() //Para fechar e poder tornar um objeto
+    des.stroke()  //Risca linha
+    des.fill()  //Pinta Área
+    }
+
+    mover(){
+        this.y += 3
+        if(this.y >=610){
+            this.y= -60
+        }
+
+    }
+}
+
+
+
+
+//classe carro
+
+//classe estrada
+
+//classe texto
+class Text{
+    des_text(text,x,y,cor,font){
+        des.fillStyle = cor
+        des.lineWidth = '5'
+        des.font = font
+        des.fillText(text,x,y)
+    }
+}
---
+++ car.js
@@ -0,0 +1,119 @@
+//leitura do canvas
+let des = document.getElementById('des').getContext('2d')
+
+//definição das instancias
+const f1 = new F1(260,460,45,100, 'assets/img/poke.png')
+const c1 = new Car(100,100,45,100, 'assets/img/bola.png')
+const c2 = new Car(150,400,45,100, 'assets/img/chari.png')
+const c3 = new Car(450,600,45,100, 'assets/img/chari.png')
+const bg1 = new Est_l(10,2,8,600,'white')
+const bg2 = new Est_l(580,2,8,600,'white')
+const bg3_1 = new Est_c(291,0,8,80,'yellow')
+const bg3_2 = new Est_c(291,320,8,80,'yellow')
+
+//definição de textos
+const t1 = new Text()
+const t2 = new Text()
+const t3 = new Text()
+const t4 = new Text()
+
+//definição d instancia de som
+const som1 = new Audio('assets/audio/motor_11.wav')
+som1.volume = 1.0
+som1.loop = true 
+
+const som2 = new Audio('assets/audio/batida_carro_.mp3')
+som2.volume = 1.0
+
+
+
+//controle de movimentação
+document.addEventListener('keydown',(event)=>{
+    if(event.key === 'a'){
+        f1.dir -=5
+    }else if(event.key === 'd'){
+        f1.dir +=5 
+    }
+    som1.play()
+})
+
+
+document.addEventListener('keyup',(event)=>{
+    if(event.key === 'a'){
+        f1.dir = 0 
+    }else if(event.key === 'd'){
+        f1.dir = 0
+    }
+})
+//game over
+
+//pontos
+    function pontos(){
+        if(f1.point(c1)){
+            f1.pts += 1
+            console.log(f1.pts)
+        }
+    }
+    function pontos(){
+        if(f1.point(c2)){
+            f1.pts += 1
+            console.log(f1.pts)
+        }
+    }
+
+    function pontos(){
+        if(f1.point(c3)){
+            f1.pts += 1
+            console.log(f1.pts)
+        }
+    }
+
+//colisão
+function colisao(){
+    if(f1.colid(c1)){
+        console.log('colidiu 1')
+        som1.pause()
+        som2.play()
+    }else if (f1.colid(c2)){
+        console.log('coidiu 2')
+        som1.pause()
+        som2.play()
+    }
+}
+//desenha
+function desenha(){
+    bg3_1.des_obj()
+    bg3_2.des_obj()
+    bg2.des_obj()
+    bg1.des_obj()
+    c1.des_obj()
+    c2.des_obj()
+    c3.des_obj()
+    t1.des_text('Pontos:',40,40,'yellow','20px Times')
+    t2.des_text('vidas:',450,40,'orange','20px Times')
+    t3.des_text(f1.pts,103,40,'yellow','20px Times')
+    t4.des_text('10',500,40,'orange','20px Times')
+    f1.des_obj()
+}
+//atualiza
+function atualiza(){
+    c1.mover()
+    c2.mover()
+    c3.mover()
+    f1.mover()
+    colisao()
+    pontos()
+    bg3_1.mover()
+    bg3_2.mover()
+    
+}
+
+//principal
+function main(){
+    des.clearRect(0,0,600,600)
+    atualiza()
+    desenha()
+    requestAnimationFrame(main)
+}
+
+main()
